4340380f139795d9a665d8d24d5d7a1cbdf52ae6
Franck-DK <franck.dang.kum-1@ou.edu>
2024-02-05 16:12:26 -0600

    Add project files.

---
+++ LC0206/Data/StuDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace a
+{
+    public class StuDB:DbContext
+    {
+        // define database table 
+        public DbSet<Student> students { get; set; } //One table in the database
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+          
+            options.UseSqlite("Data Source=Students.db");// Data Source=path to the database file
+        }
+    }
+}
---
+++ LC0206/Program.cs
@@ -0,0 +1,15 @@
+﻿// MIS 3033 003
+// FEB 06, 2024
+// Franck Dang Kum
+//113537021
+
+// Step 1: 
+Console.WriteLine("DB");
+
+// Step 2: <StartWorkingDirectory>$(MSBuildProjectDirectory)</StartWorkingDirectory>
+// Step 3: add 3 packages
+// Microsoft.EntityFrameworkCore.Design
+// Microsoft.EntityFrameworkCore.Tools
+// Microsoft.EntityFrameworkCore.Sqlite
+
+// Step 4: Design DB table 
